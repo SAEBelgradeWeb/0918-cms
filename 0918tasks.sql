e1415213b61726edb057e532b12d745c6d386fc6
Vladimir Lelicanin <vladimir.lelicanin@origin-trail.com>
2019-07-21 14:27:51 +0200

    Update 0918tasks.sql

--- 0918tasks.sql
+++ 0918tasks.sql
@@ -1,16 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.7.22)
-# Database: 0918tasks
-# Generation Time: 2019-07-21 12:05:57 +0000
-# ************************************************************
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
